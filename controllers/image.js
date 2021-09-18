d1d94beee8cc5942b281678827bbe6cd37a43477
mismo-2u <nirdogspirit@gmail.com>
2021-09-18 22:05:46 +0300

    Adding server-s files

---
+++ controllers/image.js
@@ -0,0 +1,27 @@
+import Clarifai from 'clarifai';
+
+let app = new Clarifai.App({apiKey: '4e6d0ec57f204cb99072d63f71c453ef'});
+
+export const handleApiCall = function(req,res){
+	app.models.predict({id:'f76196b43bbd45c99b4f3cd8e8b40a8a', 
+                    version:'45fb9a671625463fa646c3523a3087d5'}, 
+                    req.body.input)
+    		  .then(data=>{
+    		  	res.json(data);
+    		  })
+    		  .catch(err=>res.status(400).json('could not work w Api'))   
+}
+
+
+export const handleImage = function(req,res,db){
+	const {id} = req.body;
+	db('users').where('id','=',id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries=>{
+		res.json(entries[0])
+	})
+	.catch(err=>res.status(400).json('could not increment the count'))
+}
+// export handleImage;
+// export default {handleApiCall,handleImage}
---
+++ server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+const app = express();
+import fs from 'fs';
+import bcrypt from 'bcrypt-nodejs';
+import cors from 'cors';
+import knex from 'knex';
+import handleRegister from './controllers/register.js';
+import handleSignin from './controllers/signin.js';
+import handleProfileGet from './controllers/profile.js';
+import {handleImage} from './controllers/image.js';
+import {handleApiCall} from './controllers/image.js';
+
+
+const db = knex ({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'ds1',
+    database : 'eyedetection'
+  }
+});
+
+db.select('*').from('users').then(data=>{
+	console.log(data);
+});
+
+app.use(express.json());
+app.use(cors());
+app.get('/',(req,res)=>{
+	res.send('success');
+})
+
+app.post('/signin',(req,res)=>{handleSignin(req,res,db,bcrypt)})
+
+app.post('/register',(req,res)=>{handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id',(req,res)=>{handleProfileGet(req,res,db)})
+
+app.put('/image',(req,res)=>{handleImage(req,res,db)})
+
+app.post('/imageurl',(req,res)=>{handleApiCall(req,res)})
+
+app.listen(3000,()=>{console.log('sever good to go')});

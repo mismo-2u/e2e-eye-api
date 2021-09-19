24d3e2b810c3ef229ddc3a617c84332b740cf26b
mismo-2u <nirdogspirit@gmail.com>
2021-09-20 02:56:11 +0300

    adding db connections

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ import {handleApiCall} from './controllers/image.js';
 const db = knex ({
   // process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
   client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 

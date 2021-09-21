b94441a2e817e8a6747654a11ebfa6dce5eb6379
mismo-2u <nirdogspirit@gmail.com>
2021-09-21 23:05:31 +0300

    Dbug

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ import {handleApiCall} from './controllers/image.js';
 
 
 const db = knex ({
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+  // process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
   client: 'pg',
   connectionString: process.env.DATABASE_URL,
     ssl: true
-    // {
-    //   rejectUnauthorized: false
-    // }
+    {
+      rejectUnauthorized: false
+    }
 });
 
 db.select('*').from('users').then(data=>{

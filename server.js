7c7a89a282b050217e5a41adf93adb76b83a4666
mismo-2u <nirdogspirit@gmail.com>
2021-09-22 22:01:12 +0300

    dbug

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import handleProfileGet from './controllers/profile.js';
 import {handleImage} from './controllers/image.js';
 import {handleApiCall} from './controllers/image.js';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex ({
   client: 'pg',
     connection:{
